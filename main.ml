e8b03e52726e3f1ddd148c5b32bf91efc68e33a3
sunnyolive108 <oc.anderson.oa@gmail.com>
2025-01-08 21:07:47 +0100

    trying my best here

---
+++ main.ml
@@ -0,0 +1,11 @@
+open Analyze
+
+let save_transactions filename = 
+  let json = !transactions |> [%to_yojson: transaction list] in 
+  Out_channel.write_all filename ~data:(Yojson.Basic.pretty_to_string json)
+
+ let load_transactions filename =
+   try
+     let json = Yojson.Basic.from_file filename in
+     transactions := json |> [%of_yojson: transaction list] |> Result.ok_or_failwith
+   with _ -> printf "Error loading transactions\n"
